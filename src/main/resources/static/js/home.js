26da46f92e3ef2aa317d6966c17e04927d0c72fe
ChenDanni <danniChen96@163.com>
2017-04-24 21:33:08 +0800

    Update

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -47,9 +47,12 @@ $(document).ready(function(){
     $(document).on("click","#new-note-btn",function () {
         var btn = $(this);
         var note_name = btn.prev().val();
-        console.log(note_name);
+        // console.log(note_name);
 
-        $("#editing").append(note_name);
+        // $("#editing").append(note_name);
+        $("#editing").val(note_name);
+        $("#content").val("");
+        $('#preview').html(marked($('#content').val()));
 
     });
 
@@ -57,15 +60,22 @@ $(document).ready(function(){
         var btn = $(this);
         var noteId = btn.next().val();
         var data = {"noteId":noteId};
+
+        $("a.active").removeClass("active");
         btn.addClass("active");
+
         jQuery.ajax({
             async: false,
             data: data,
             url: "/note/show",
             type: "get",
             success: function (msg) {
-                btn.addClass("active");
+                // btn.addClass("active");
                 console.log(msg);
+                $("#content").val(msg["content"]);
+                $("#editing").val(msg["name"]);
+                $('#preview').html(marked($('#content').val()));
+
             },
             error: function () {
                 console.log("err in show");
@@ -75,12 +85,15 @@ $(document).ready(function(){
 
     $(document).on("click","#create-btn",function () {
 
-        var name = $("#editing").innerHTML;
-        var dirId = "";
-        var dirName = $(this).prev().val();
-        var content = "";
+        var name = $("#editing").val();
+        var dirId = $("#select-dir").val();
+        var dirName = $("#select-dir").find("option:selected").text();
+        var content = $("#content").val();
 
         console.log(name);
+        console.log(dirId);
+        console.log(dirName);
+        console.log(content);
 
         var data = {
             "name":name,
@@ -88,18 +101,41 @@ $(document).ready(function(){
             "dirName":dirName,
             "content":content
         };
-        // jQuery.ajax({
-        //     async: false,
-        //     data: data,
-        //     url: "/note/add",
-        //     type: "get",
-        //     success: function (msg) {
-        //         console.log(msg);
-        //     },
-        //     error: function () {
-        //         console.log("err in add");
-        //     }
-        // });
+        jQuery.ajax({
+            async: false,
+            data: data,
+            url: "/note/add",
+            type: "post",
+            success: function (msg) {
+                console.log(msg);
+            },
+            error: function (data) {
+                console.log(JSON.stringify(data));
+            }
+        });
+
+    });
+    $(document).on("click","#identify-btn",function () {
+
+        var content = $("#content").val();
+
+        $.ajax({
+            url: '/note/transform',
+            type: 'POST',
+            cache: false,
+            data: new FormData($('#identify-form')[0]),
+            processData: false,
+            contentType: false,
+            success: function (msg) {
+                console.log(msg);
+                content += msg;
+                $("#content").val(content);
+                $('#preview').html(marked($('#content').val()));
+            },
+            error: function () {
+                console.log("err in identify");
+            }
+        });
 
     });
 
